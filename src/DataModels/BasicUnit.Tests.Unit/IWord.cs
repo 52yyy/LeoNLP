1c30f56d88ef75a31e2d85ed5516680e3a4cc6aa
Leo He <13488835500@163.com>
2016-10-24 11:54:34 +0800

    添加句子类和词类

---
+++ src/DataModels/BasicUnit.Tests.Unit/IWord.cs
@@ -0,0 +1,18 @@
+﻿namespace BasicUnit.Tests.Unit
+{
+	/// <summary>
+	///		词接口
+	/// </summary>
+	public interface IWord
+	{
+		/// <summary>
+		///		词形
+		/// </summary>
+		string Name { get; set; }
+
+		/// <summary>
+		///		词性
+		/// </summary>
+		string Pos { get; set; }
+	}
+}
